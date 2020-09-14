31a96cbc581c3c76f9ada0b138b8b507860d5497
Guilherme Raimondi <raimondi.it@gmail.com>
2020-09-14 14:43:14 -0300

    project base structure

---
+++ src/main/kotlin/com/tqi/pagseguroprodutos/PagseguroProdutosApplication.kt
@@ -0,0 +1,11 @@
+package com.tqi.pagseguroprodutos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PagseguroProdutosApplication
+
+fun main(args: Array<String>) {
+	runApplication<PagseguroProdutosApplication>(*args)
+}
