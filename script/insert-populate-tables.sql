ef8608933f817eba0bb53c13cd3441dc13d60a34
Guilherme Raimondi <raimondi.it@gmail.com>
2020-09-17 09:42:53 -0300

    populating db via docker-compose

---
+++ script/insert-populate-tables.sql
@@ -0,0 +1,14 @@
+INSERT INTO pags_product.product (id, name, category, price, active)
+    VALUES ("84437679-d6d0-47b0-85a7-888d27e1b1d2", "produto 1", "categoria 1", 100.5, true);
+
+INSERT INTO pags_product.product (id, name, category, price, active)
+    VALUES ("e698504b-2b80-40af-807e-398dfdb4d678", "produto 2", "categoria 1", 11.5, true);
+
+INSERT INTO pags_product.product (id, name, category, price, active)
+    VALUES ("a13104de-53b7-4946-a6ba-80d05776575d", "produto 3", "categoria 2", 54.5, true);
+
+INSERT INTO pags_product.product (id, name, category, price, active)
+    VALUES ("c5a69a22-b557-4461-a51d-32b872c2ddf6", "produto 4", "categoria 1", 32.5, true);
+
+INSERT INTO pags_product.product (id, name, category, price, active)
+    VALUES ("e67204f3-af2d-4144-93fa-9973d09c92be", "produto 5", "categoria 2", 19.5, false);
